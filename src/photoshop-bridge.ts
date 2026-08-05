b8336b86d7fff785638cc67697f1a756dcf070cf
Ludwig Zeller <contact@ludwigzeller.net>
2026-08-05 09:11:37 +0200

    license headers

--- src/photoshop-bridge.ts
+++ src/photoshop-bridge.ts
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2026 Sphericals
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * This file is part of Mega Musa.
+ *
+ * Mega Musa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Mega Musa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mega Musa. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { resampleGray } from "./image-codec";
 
 const { app, action, core, imaging } = require("photoshop");
--- src/uxp.d.ts
+++ src/uxp.d.ts
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2026 Sphericals
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * This file is part of Mega Musa.
+ *
+ * Mega Musa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Mega Musa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mega Musa. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // Minimal ambient typings for the UXP runtime modules we use. These are
 // provided by Photoshop at load time (kept `external` in the bundle), so we
 // only declare the surface this plugin touches. Loosely typed on purpose.
